af43ba97c1ffdb9c9774bc1bf8aaecd1a6f137e4
Pachi <chiara.palmieri@gmail.com>
2018-12-19 17:56:54 +0100

    first import

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  use_doorkeeper
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'application#me'
+  get '/me' => 'application#me'
+end
